3ab3f9e5f5513788aa89b382c17333e0ac5fc089
Baatchia Riyadh <Baatchia_Riyadh@protonmail.com>
2022-10-25 02:39:37 +0100

    Migrated to a client only build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { v4 as uuid } from 'uuid';
 import './App.css';
 import Card from './components/Card';
